95e1b58158ca0744f038935d15c78fa0a8258df8
Flowyk <flowyk@gmail.com>
2021-07-18 19:29:05 +0200

    Spring Init

---
+++ src/main/java/com/flowyk/clvowatch/ClvoWatchApplication.java
@@ -0,0 +1,13 @@
+package com.flowyk.clvowatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClvoWatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClvoWatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/flowyk/clvowatch/ClvoWatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flowyk.clvowatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClvoWatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
